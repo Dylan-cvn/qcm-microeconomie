6929e1bc6433284c5c2e507ea61ec98d225da1f0
Dylan-cvn <d.cavin@outlook.com>
2025-11-04 12:04:50 +0100

    Change highlight color from red to yellow

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ QUESTIONS = [
             "En concurrence parfaite, le prix du marché (P) = au coût marginal (Cm) = à la recette marginal (Rm)." 
             "Le producteur étant preneur de prix en concurrence parfaite veut maximiser son profit."
             ),
-        "highlight_color": "#ff4b4b" # rouge
+        "highlight_color": "#ffc107",  # jaune
     }     
 ]
 
