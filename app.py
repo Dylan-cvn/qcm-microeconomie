7451efefc42357a4504f2b2a533d994912257653
Dylan-cvn <d.cavin@outlook.com>
2025-12-22 17:50:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -735,10 +735,10 @@ QUESTIONS = [
     },
     {
         "q": (
-            "Sur le marché des tomates, la fonction d'offre est la suivante :\n\n"
-            "Qᵒ = 220 − 70P − 12ω + 80T\n\n"
+            "Sur le marché des tomates, la fonction d'offre est la suivante : "
+            "Qᵒ = 220 − 70P − 12ω + 80T "
             "Où Qᵒ représente la quantité offerte de tomates (en kilos), P le prix des tomates (en CHF par kilo), ω le salaire horaire des travailleurs (en CHF) & "
-            "T un indicateur relatif à la technologie utilisée. Si ω = 25 & T = 1.\n\n"
+            "T un indicateur relatif à la technologie utilisée. Si ω = 25 & T = 1. "
             "Quel est le SP lorsque P = 7 CHF par kilo ?"
         ),
         "choices": [
