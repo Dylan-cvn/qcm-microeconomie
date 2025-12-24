7b59c668cca28e6e7c5d75cd6144492f62fb3cd3
Dylan-cvn <d.cavin@outlook.com>
2025-12-24 13:04:30 +0100

    Add newline character in marketing scenario text

--- app.py
+++ app.py
@@ -660,7 +660,7 @@ QUESTIONS = [
     },
     {
         "q": (
-            "Marianne, responsable marketing d'une petite entreprise, dépose chaque matin sa fille à la crèche avant de prendre la route au volant de sa voiture pour se rendre à son bureau. "
+            "Marianne, responsable marketing d'une petite entreprise, dépose chaque matin sa fille à la crèche avant de prendre la route au volant de sa voiture pour se rendre à son bureau. \n"
             "À propos de son salaire de réserve ω_res, voici une liste d'affirmations : \n"
             "i. Si tarifs de la crèche ↑ al. ω_res ↓. \n"
             "ii. Si prix du carburant utilisé pour le déplacement ↑ al. ω_res ↓. \n"
